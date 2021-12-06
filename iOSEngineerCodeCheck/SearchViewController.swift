de323d81d14c8a0013225c25e65db03e6a2eb7ae
arai <niimaru09@gmail.com>
2021-12-06 15:06:17 +0900

    UISearchBarDelegateを継承ではなく拡張にすることで、記述場所を分割しました

--- iOSEngineerCodeCheck/SearchViewController.swift
+++ iOSEngineerCodeCheck/SearchViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SearchViewController: UITableViewController, UISearchBarDelegate {
+class SearchViewController: UITableViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     
@@ -23,33 +23,7 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
         searchBar.text = "GitHubのリポジトリを検索できるよー"
         searchBar.delegate = self
     }
-    
-    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
-        searchBar.text = ""
-        return true
-    }
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        task?.cancel()
-    }
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchText = searchBar.text!
-        guard searchText.count != 0 else { return }
-        
-        url = "https://api.github.com/search/repositories?q=\(searchText!)"
-        task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
-            guard let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any],
-                  let items = obj["items"] as? [[String: Any]] else { return }
-            self.repositories = items
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
-            
-        }
-        task?.resume()
-    }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "Detail" {
             let detailVC = segue.destination as! DetailViewController
@@ -73,6 +47,38 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         index = indexPath.row
         performSegue(withIdentifier: "Detail", sender: self)
+    }
+    
+}
+
+
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.text = ""
+        return true
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        task?.cancel()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchText = searchBar.text!
+        guard searchText.count != 0 else { return }
+
+        url = "https://api.github.com/search/repositories?q=\(searchText!)"
+        task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
+            guard let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any],
+                  let items = obj["items"] as? [[String: Any]] else { return }
+            self.repositories = items
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+
+        }
+        task?.resume()
     }
     
 }
